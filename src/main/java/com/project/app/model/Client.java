1eccd0eab5dca31028478c8cfb718e979d4ac11a
236370 <ixior@ixior-Lenovo-ideapad-700-15ISK>
2018-11-27 21:53:21 +0100

    Client class expanded and ClientAccesor filled up with methods

--- src/main/java/com/project/app/model/Client.java
+++ src/main/java/com/project/app/model/Client.java
@@ -4,25 +4,44 @@ public class Client {
 
     /**
      * @author      Paweł Krupski
-     * @info        Class that represents each single Client that registered in app
-     * @version     1.0
+     * @info        Class that represents each single Client that registered in app,
+     *              Added elo and level
+     * @version     1.1
      */
 
     private String login;
     private String password;
+    private String email;
+    private int elo;
+    private String level;
 
-    public Client( String login, String password){
+    public Client( String login, String password, String email, String level, int elo){
         this.login = login;
         this.password = password;
-
+        this.email = email;
+        this.elo = elo;
+        this.level = level;
     }
 
-    public String getLogin() {
-        return login;
-    }
+    public String getLogin() { return login; }
 
     public String getPassword() {
         return password;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getElo() {
+        return elo;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+
+
 
 }
--- src/main/java/com/project/app/model/ClientsDataAccessor.java
+++ src/main/java/com/project/app/model/ClientsDataAccessor.java
@@ -1,7 +1,28 @@
 package com.project.app.model;
 
 public class ClientsDataAccessor {
-    //Class where we access Client Logs, Pass and Statistics
-    //TODO : everything
 
+    /**
+     * @author      Paweł Krupski
+     * @info        Class that allows to get access to client database 
+     * @version     1.0
+     */
+
+    private ClientsJSONHandler handler;
+
+    public ClientsDataAccessor() {
+        this.handler = new ClientsJSONHandler();
+    }
+
+    public void addClienttoJSON(Client client){
+        handler.addClientToJSON(client);
+    }
+
+    public Client getClient(String login){
+        return handler.getClient(login);
+    }
+
+    public boolean clientAlreadyExist(String login){
+        return handler.clientAlreadyExist(login);
+    }
 }
