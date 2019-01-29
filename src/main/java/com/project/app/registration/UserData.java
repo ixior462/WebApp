7e331d40759b0d9cd189941e2a9ce526f633d745
weronika997 <236740@student.pwr.edu.pl>
2019-01-29 23:43:59 +0100

    deleted unused html file, added documentation comments

--- src/main/java/com/project/app/registration/UserData.java
+++ src/main/java/com/project/app/registration/UserData.java
@@ -1,50 +1,83 @@
 package com.project.app.registration;
-
+/**
+ *  Class used to transfer user data entered during registration
+ * @author      Weronika Pawlak
+ *  @version     1.0
+ */
 
 public class UserData {
-    /**
-     * @author      Weronika Pawlak
-     *         Class used to transfer user data entered during registration
-     * @version     1.0
-     */
+
     public String userName;
     public String password;
     public String matchingPassword;
     public String email;
 
+    /**
+     * Method that returns client's username
+     * @return client's username
+     * @author Weronika Pawlak
+     */
     public String getUserName() {
 
         return userName;
     }
-
+    /**
+     * Method that returns client's email
+     * @return client's email
+     * @author Weronika Pawlak
+     */
     public String getEmail() {
 
         return email;
     }
-
+    /**
+     * Method that returns client's matching password
+     * @return client's matching password
+     * @author Weronika Pawlak
+     */
     public String getMatchingPassword() {
 
         return matchingPassword;
     }
-
+    /**
+     * Method that returns client's password
+     * @return client's password
+     * @author Weronika Pawlak
+     */
     public String getPassword() {
 
         return password;
     }
-
+    /**
+     * Method that sets client's email
+     * @param new email value
+     * @author Weronika Pawlak
+     */
     public void setEmail(String email) {
         this.email = email;
     }
-
+    /**
+     * Method that sets client's matching password
+     * @param new matching password value
+     * @author Weronika Pawlak
+     */
     public void setMatchingPassword(String matchingPassword) {
 
         this.matchingPassword = matchingPassword;
     }
-
+    /**
+     * Method that sets client's password
+     * @param new password value
+     * @author Weronika Pawlak
+     */
     public void setPassword(String password) {
         this.password = password;
     }
-
+    /**
+     * Method that sets client's username
+     * @param new username value
+     * @author Weronika Pawlak
+     */
     public void setUserName(String userName) {
 
         this.userName = userName;
