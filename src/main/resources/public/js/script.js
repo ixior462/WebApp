5734e82fa491a0a674ad9cd9cb25280f5c90db7b
Kuba <kyuuba97@gmail.com>
2019-01-30 10:57:23 +0100

    Level labels in menu, debugging, correcting styles

--- src/main/resources/public/js/script.js
+++ src/main/resources/public/js/script.js
@@ -21,12 +21,43 @@ function open() {
     document.getElementById("learn").style.transition = "0.5s";
 }
 var lessons = 0;
+var numbOfLessons = [];
 function initializeMenu() {
     var cont = document.getElementById("sideMenu");
     console.log(lessons);
     for (var i = 1; i <= lessons; i++) {
         (function f(i) {
             {
+                if(i==1){
+                    var level = document.createElement("p");
+                    level.innerText="A1";
+                    cont.appendChild(level);
+                }
+                else if(i-1==numbOfLessons[0]){
+                    var level = document.createElement("p");
+                    level.innerText="A2";
+                    cont.appendChild(level);
+                }
+                else if(i-1==numbOfLessons[1]){
+                    var level = document.createElement("p");
+                    level.innerText="B1";
+                    cont.appendChild(level);
+                }
+                else if(i-1==numbOfLessons[2]){
+                    var level = document.createElement("p");
+                    level.innerText="B2";
+                    cont.appendChild(level);
+                }
+                else if(i-1==numbOfLessons[3]){
+                    var level = document.createElement("p");
+                    level.innerText="C1";
+                    cont.appendChild(level);
+                }
+                else if(i-1==numbOfLessons[4]){
+                    var level = document.createElement("p");
+                    level.innerText="C2";
+                    cont.appendChild(level);
+                }
                 var link = document.createElement("pre");
                 link.innerText = "Lekcja " + i;
                 link.onclick = function () {
